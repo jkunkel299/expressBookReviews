9c088bb775a4cbcf15ac4f41d00dfc8841810713
Jessica Kunkel <jmk299pitt@gmail.com>
2023-09-18 14:28:23 -0400

    Completed Tasks 10-13 for promises/callbacks, added descriptive comments to API calls

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,6 +5,7 @@ const regd_users = express.Router();
 
 let users = [];
 
+// determine if username is valid. Used in general.js when determining if a username is already taken
 const isValid = (username)=>{ //returns boolean
     let userswithsamename = users.filter((user)=>{
         return user.username === username
@@ -16,6 +17,7 @@ const isValid = (username)=>{ //returns boolean
     }
 }
 
+//returns boolean value for whether a user is within the list of valid users
 const authenticatedUser = (username,password)=>{ //returns boolean
     let validusers = users.filter((user)=>{
         return (user.username === username && user.password === password)
@@ -31,11 +33,13 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 regd_users.post("/login", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
-  
+    
+    // if either username or password is null, cannot log in
     if (!username || !password) {
         return res.status(404).json({message: "Error logging in"});
     }
   
+    // if username/password pair are within registered users list, grants the user JWT access token
     if (authenticatedUser(username,password)) {
         let accessToken = jwt.sign({
             data: password
@@ -46,7 +50,7 @@ regd_users.post("/login", (req,res) => {
         }
         
         return res.status(200).send("User successfully logged in");
-
+    // if the username/password pair are not within the registered users list, user cannot log in
     } else {
         return res.status(208).json({message: "Invalid Login. Check username and password"});
     }
@@ -54,18 +58,20 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    // access "customer" from index.js to get session username
     let isbn = req.params.isbn;
     let review = req.query.review; 
+    // access "customer" from index.js to get session username
     let customer = req.session.authorization.username;
     
     books[isbn].reviews[customer] = review;
     res.send("Updated book with ISBN "+isbn+" with review: "+review+" for user "+customer+" successfully");
 });
 
+// Delete a book review
 regd_users.delete("/auth/review/:isbn",(req,res)=>{
     let isbn = req.params.isbn;
     let customer = req.session.authorization.username;
+    // if the customer has a review for this book (by ISBN), deletes the customer's review for that book
     if(books[isbn].reviews[customer]){
         delete books[isbn].reviews[customer]
     }
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const axios = require('axios'); // requiring axios package
+
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
+// Register new user with username and password
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -21,26 +23,42 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
+/* Task 1: get entire book list
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify({books},null,4));
-});
+    res.send(JSON.stringify({books},null,4)); 
+});*/
+
+/* Task 10: get entire book list using promises */
+public_users.get('/', function(req,res){
+    Promise.resolve(books)
+        .then(result => res.status(200).send(result))
+        .catch(err => res.status(500).send(err));
+})
 
 // Get book details based on ISBN
+/* Task 2: Get book by ISBN 
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     res.send(books[isbn]);
-});
+});/*
+
+/* Task 11: get book by ISBN using promises */
+public_users.get('/isbn/:isbn', function(req,res){
+    const isbn = req.params.isbn;
+    Promise.resolve(books[isbn])
+        .then(result => res.status(200).send(result))
+        .catch(err => res.status(500).send(err));
+})
   
 // Get book details based on author
+/* Task 3: Get book by author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
     
     var elements = Object.keys(books).map(function(k) {
         return books[k];
     })
-       
-    //res.send(elements);
-    
+
     let filteredBooks = [];
     for (let i= 0; i<elements.length; i++) {
         if (elements[i].author === author ) {
@@ -48,17 +66,44 @@ public_users.get('/author/:author',function (req, res) {
         }
     }
     res.send(filteredBooks);
-});
+});*/
+
+/* Task 12: Get book by author using promises */
+public_users.get('/author/:author',function (req, res) {
+    var getByAuthor = new Promise(function(resolve,reject){
+        // get author from request parameters
+        const author = req.params.author;
+        
+        // create list of books object keys (ISBN). This is an array that can be manipulated and filtered as normal, as books is an associative array and cannot be filtered using a for loop
+        var elements = Object.keys(books).map(function(k) {
+            return books[k];
+        })
+        
+        //create empty array for filtered books
+        let filteredBooks = [];
+        // loop through list of books (elements) and filter by author matching that which was requested
+        for (let i= 0; i<elements.length; i++) {
+            if (elements[i].author === author ) {
+                filteredBooks= [...filteredBooks, elements[i]];
+            }
+        }
+        resolve(filteredBooks);
+    })
+    
+    getByAuthor
+    .then(result => res.status(200).send(result))
+    .catch(err => res.status(500).send(err));
+})
+
 
 // Get all books based on title
+/* Task 4: Get book by title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     
     var elements = Object.keys(books).map(function(k) {
         return books[k];
     })
-       
-    //res.send(elements);
     
     let filteredBooks = [];
     for (let i= 0; i<elements.length; i++) {
@@ -67,7 +112,36 @@ public_users.get('/title/:title',function (req, res) {
         }
     }
     res.send(filteredBooks);
-});
+});*/
+
+/* Task 13: Get book by title using promises */
+public_users.get('/title/:title',function (req, res) {
+    var getByTitle = new Promise(function(resolve,reject){
+    
+       // get title from request parameters
+       const title = req.params.title;
+        
+       // create list of books object keys (ISBN). This is an array that can be manipulated and filtered as normal, as books is an associative array and cannot be filtered using a for loop
+       var elements = Object.keys(books).map(function(k) {
+           return books[k];
+       })
+       
+       //create empty array for filtered books
+       let filteredBooks = [];
+       // loop through list of books (elements) and filter by author matching that which was requested
+        for (let i= 0; i<elements.length; i++) {
+            if (elements[i].title === title ) {
+                filteredBooks= [...filteredBooks, elements[i]];
+            }
+        }
+        resolve(filteredBooks);
+    })
+    
+    getByTitle
+    .then(result => res.status(200).send(result))
+    .catch(err => res.status(500).send(err));
+})
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
